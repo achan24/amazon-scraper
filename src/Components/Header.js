7249be3981b101cb5d197243eb24a800a3e91e0a
achan80 <chan.albert@gmail.com>
2024-03-20 11:38:33 +0000

    finished header structure

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,19 @@
+import logo from "../images/magnifying-glass2.png"
+
 const Header = () => {
 
-  const today = new Date().toString.slice(0, 10)
+  const today = new Date().toString().slice(0, 10)
+  
 
 
   return ( 
     <header>
-      <div>
+      <div className="text-container">
         <h1>Dealfinder</h1>
         <p>{today}</p>
+      </div>
+      <div className="logo-container">
+        <img src={logo} alt="logo" />
       </div>
     </header>
     
